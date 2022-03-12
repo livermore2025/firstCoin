7a1210f61bd8547ec20609f9a23118d403e72ca6
livermore2025 <livermore2025@gmail.com>
2022-03-13 00:44:13 +0900

    v01

--- blockchain/dev/blockchain.js
+++ blockchain/dev/blockchain.js
@@ -4,6 +4,7 @@ function Blockchain() {
 }
 
 Blockchain.prototype.createNewBlock = function (
+  // 새로운 블록 만드는 메소드. 이 블록 안에는 트랜잭션들이 저장되어 있고 이 TX들은 마지막 블록을 채굴한 이후 생성된 TX
   nonce,
   previousBlockHash,
   hash
@@ -13,6 +14,11 @@ Blockchain.prototype.createNewBlock = function (
     index: this.chain.length + 1, // newBlock이 몇번째 블록인지 index속성 추가
     timestamp: Date.now(), // 블록 생성 시점
     transactions: this.newTransactions, // 새로운 블록 만들었을 때 모든 새로운 트랜잭션들과 미결 트랜잭션들을 새로운 블록에 추가.
-    nonce: nonce,
+    nonce: nonce, // 자격증명, 단순히 어떤 숫자 값. POW를 통해 적법하게 새로운 블록을 만들었다는 증거.
+    hash: hash, // newBlock객체에서 온 값. newTransactions를 해싱 함수에 매개변수로 전달함.
+    previousBlockHash: previousBlockHash, // hash와 유사. hash가 현재라면 previousBlockHash는 이전 블록의 데이터, 또는 이전 블록에서 현재 블록까지의 데이터를 해싱한 값을 의미.
   };
+  this.newTransactions = []; // 다음 블록 생성을 위해 배열 초기화
+  this.chain.push(newBlock); // 블록을 체인에 추가
+  return newBlock; // 반환
 };
